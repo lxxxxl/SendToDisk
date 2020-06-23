f31145c0148d3c674cb5dc5854ac5786e8ae2214
lxxxxl <lxxxxl>
2020-06-23 20:46:00 +0300

    Missing permission fix: READ_EXTERNAL_STORAGE

--- app/src/main/java/lxx/ru/sendtodisk/MainActivity.java
+++ app/src/main/java/lxx/ru/sendtodisk/MainActivity.java
@@ -1,8 +1,9 @@
 package lxx.ru.sendtodisk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.content.Intent;
+import android.Manifest;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions(this, permissions, 1);
 
     }
 }
